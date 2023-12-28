833d9ab3929aa3ed18b23dbe69070fcd3a741a78
AkiKurvinen <aki.kurvinen@hotmail.fi>
2023-12-28 13:21:34 +0200

    add sb themes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,54 @@
-/** @type { import('@storybook/nextjs').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
+const config: StorybookConfig = {
   stories: [
+    '../app/**/*.mdx',
+    '../app/**/*.stories.@(js|jsx|ts|tsx)',
     "../app/[lng]/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+  features: {
+    buildStoriesJson: true,
+  },
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling',
+    '@storybook/addon-viewport',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-themes',
+    '@storybook/addon-a11y',
+    '@storybook/addon-designs',
   ],
   framework: {
-    name: "@storybook/nextjs",
-    options: {},
+    name: '@storybook/nextjs',
+    options: {
+      nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+    },
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+
+      return test.test('.svg');
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,67 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
+import { Preview, ReactRenderer } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { light, dark, muiDefault, muiGold } from '../themes/all_themes';
+import { chromaticThemeModes } from './modes';
+
+export const decorators = [
+  withThemeFromJSXProvider<ReactRenderer>({
+    themes: {
+      light: light,
+      dark: dark,
+      muiDefault: muiDefault,
+      muiGold: muiGold,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  }),
+];
+
+const preview: Preview = {
+  globalTypes: {
+    is_chromatic: { value: false },
+  },
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        desktop: {
+          name: 'desktop',
+          styles: { width: '1920px', height: '1080px' },
+        },
+        mobile: { name: 'mobile', styles: { width: '200px', height: '400px' } }, // Viewport width range [200, 2560]
+        defaultViewport: 'responsive',
       },
     },
+    chromatic: {
+      modes: chromaticThemeModes,
+      diffThreshold: 0.063, // default 0.063
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+
+    backgrounds: {
+      values: [
+        {
+          name: 'light',
+          value: light.palette.background.paper,
+        },
+        {
+          name: 'dark',
+          value: dark.palette.background.paper,
+        },
+        {
+          name: 'muiGold',
+          value: muiGold.palette.background.paper,
+        },
+        {
+          name: 'muiDefault',
+          value: muiDefault.palette.background.paper,
+        },
+      ],
+    },
+    decorators: decorators,
   },
 };
 
