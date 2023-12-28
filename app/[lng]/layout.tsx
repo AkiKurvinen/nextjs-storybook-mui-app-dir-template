ba6faf61c54b6e603994e0fd62e8054e4b470063
AkiKurvinen <aki.kurvinen@hotmail.fi>
2023-12-28 13:56:19 +0200

    init mui themes

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -3,6 +3,10 @@ import './global.css'
 import { dir } from 'i18next'
 import { languages } from '../i18n/settings'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider, THEME_ID, createTheme } from '@mui/material/styles';
+import {Theme} from '@mui/material'
+import {dark} from '../../themes/all_themes'
+import MUIThemeProvider from '../helpers/MUIThemeProvider';
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }))
@@ -14,11 +18,11 @@ export default function RootLayout({
     lng
   }
 }) {
-  return (
+  return ( 
     <html lang={lng} dir={dir(lng)}>
       <head />
       <body>
-      <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      <MUIThemeProvider>{children}</MUIThemeProvider>
       </body>
     </html>
   )
