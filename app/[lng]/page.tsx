2c340343cb9c01d67b64beb7216c66fa5dd54f1e
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2023-12-31 14:29:59 +0200

    modify RootLayout

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -5,13 +5,14 @@ import { StockPanelController } from '../components/organisms/StockPanel/StockPa
 import { Footer } from '../components/organisms/Footer/Footer';
 import { WeatherWidget } from '../components/organisms/WeatherWidget/WeatherWidget';
 import { NavController } from '../components/organisms/Nav/NavController';
+import { ThemeBar } from '../components/organisms/ThemeBar/ThemeBar';
 
 export async function generateMetadata({ params: { lng } }) {
   const { t } = await useTranslation(lng);
   return { title: t('h1') };
 }
 
-export default async function Page({ params: { lng }, props }) {
+export default async function Page({ params: { lng , ...props }}) {
   // const [admin, setAdmin] = useState(false);
 
   if (languages.indexOf(lng) < 0) lng = fallbackLng
@@ -22,6 +23,7 @@ export default async function Page({ params: { lng }, props }) {
       nav={
         <>
       <NavController lng={lng} path={'/'} {...props} />
+      <ThemeBar/>
         </>
       }
       main={<StockPanelController admin={true} lng={''} path={'/'} />}
