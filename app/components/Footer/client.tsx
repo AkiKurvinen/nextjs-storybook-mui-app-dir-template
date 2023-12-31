71f1c32453acc3afb2653273be03687f4ee8d43a
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2023-12-31 12:24:23 +0200

    add components

--- app/components/Footer/client.tsx
+++ app/components/Footer/client.tsx
@@ -1,7 +1,7 @@
 'use client'
 
+import { useTranslation } from '../../i18n/client'
 import { FooterBase } from './FooterBase'
-import { useTranslation } from '../../../i18n/client'
 
 export const Footer = ({ lng, path }) => {
   const { t } = useTranslation(lng, 'footer')
