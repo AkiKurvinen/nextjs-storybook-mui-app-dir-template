f2a2eda544f36553f59f024ddcc83daf0819e2ba
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2024-01-02 14:29:20 +0200

    initialize translations

---
+++ app/[lng]/test/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Footer } from "../../components/organisms/Footer/Footer"
+import { NavController } from "../../components/organisms/Nav/NavController"
+import { StockPanelController } from "../../components/organisms/StockPanel/StockPanelController"
+import { ThemeBar } from "../../components/organisms/ThemeBar/ThemeBar"
+import { WeatherWidget } from "../../components/organisms/WeatherWidget/WeatherWidget"
+import { PrimaryPage } from "../../components/templates/PrimaryPage/PrimaryPage"
+import { useTranslation } from "../../i18n/client"
+import { useState } from 'react'
+export default function About({...props}) {
+    const { t } = useTranslation(props.lng, 'client-page')
+    const [counter, setCounter] = useState(0)
+
+    return (
+    <PrimaryPage
+      nav={
+        <>
+      <NavController lng={props.lng} path={'/'} {...props} />
+      <ThemeBar/>
+        </>
+      }
+      main={<StockPanelController admin={true} lng={props.lng} path={'/'} />}
+      footer={
+        <Footer>
+          <WeatherWidget />
+        </Footer>
+      }
+    />
+    )
+}
